11dabe0a22726d5a780d1044dae94afbdb40f3d1
ukjmichel <jmichel.uk@outlook.fr>
2024-07-03 22:45:05 +0200

    error page url correction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { HomePage,AboutPage,LocationPage, ErrorPageNotFound } from "./assets/pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./assets/styles/index.scss";
-import Carousel from "./assets/components/Carousel";
+
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/location/:id" element={<LocationPage />} />
-        <Route path="*" element={<ErrorPageNotFound />} />
-        <Route path="test" element={<Carousel />} />
+        <Route path="*" element={<Navigate to="/error_page_not_found" />} />
+        <Route path="/error_page_not_found" element={<ErrorPageNotFound />} />
       </Routes>
     </Router>
   );
--- src/assets/pages/LocationPage.js
+++ src/assets/pages/LocationPage.js
@@ -4,52 +4,69 @@ import {
   LocationDetail,
   LocationTags,
   LocationScore,
-  Carousel,
 } from "../components";
 import "../styles/location-page.scss";
 import data from "../../data.json";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const LocationPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const [isDataExist, setIsDataExist] = useState(false);
+  
   const locationData = data.filter((item) => item.id === id)[0];
-  const {  title, description, location, equipments, tags, host,pictures } =
-    locationData;
 
-  return (
-    <Layout>
-      <section id="location-page">
-        <Carousel images={pictures} />
+  useEffect(() => {
+    if (!locationData) {
+      // Redirect to the `/error` route if the `id` is invalid
+      navigate("/error_page_not_found");
+    } else {
+      setIsDataExist(true);
+    }
+  }, [locationData, navigate]);
 
-        <div className="location-content display-mobile">
-          <div className="location-name">
-            <h1>{title}</h1>
-            <p>{location}</p>
-          </div>
-          <LocationTags tags={tags} />
-          <div className="flex-1">
-            <LocationScore />
-            <LocationAvatar name={host.name} picture={host.picture} />
-          </div>
-          <LocationDetail description={description} equipments={equipments} />
-        </div>
+  if (isDataExist) {
+    const { cover, title, description, location, equipments, tags, host } =
+      locationData;
 
-        <div className="location-content display-desktop">
-          <div className="flex-1">
+    return (
+      <Layout>
+        <section id="location-page">
+          <figure>
+            <img src={cover} alt="prewiew of a location" />
+          </figure>
+
+          <div className="location-content display-mobile">
             <div className="location-name">
               <h1>{title}</h1>
               <p>{location}</p>
             </div>
-            <LocationAvatar name={host.name} picture={host.picture} />
-          </div>
-          <div className="flex-1">
             <LocationTags tags={tags} />
-            <LocationScore />
+            <div className="flex-1">
+              <LocationScore />
+              <LocationAvatar name={host.name} picture={host.picture} />
+            </div>
+            <LocationDetail description={description} equipments={equipments} />
           </div>
-          <LocationDetail description={description} equipments={equipments} />
-        </div>
-      </section>
-    </Layout>
-  );
+
+          <div className="location-content display-desktop">
+            <div className="flex-1">
+              <div className="location-name">
+                <h1>{title}</h1>
+                <p>{location}</p>
+              </div>
+              <LocationAvatar name={host.name} picture={host.picture} />
+            </div>
+            <div className="flex-1">
+              <LocationTags tags={tags} />
+              <LocationScore />
+            </div>
+            <LocationDetail description={description} equipments={equipments} />
+          </div>
+        </section>
+      </Layout>
+    );
+  }
 };
 export default LocationPage;
